cac20d8343b5803a43a2ba2dfff232679a934c1a
cherif-asma <asmacherif144@gmail.com>
2019-11-30 15:09:25 +0100

    STATE

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+
+
+
+class App extends Component {
+
+  state = {
+    name: 'Sarra',
+    photo: 'https://www.robohash.org/sarra2?set=set3',
+    description: 'My name is Sara'
+  }
+
+  handleHaroldClick = () => {
+    this.setState({
+      name: 'Harold',
+      photo: 'https://www.robohash.org/harold2?set=set3',
+      description: 'I am a Designer'
+    })
+  }
+
+  handleDanielClick = () => {
+    this.setState({
+      name: 'Daniel',
+      photo: 'https://www.robohash.org/harold2?set=set3',
+      description: 'I am a web developer'  })
+  }
+
+  handleSarraClick = () => {
+    this.setState({
+      name: 'Sarra',
+      photo: 'https://www.robohash.org/Danie?set=set3',
+      description: 'My name is Sara'
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{marginTop: 50, display: 'flex', justifyContent: 'center'}}>
+        <button onClick={this.handleSarraClick}>Sarra</button>
+        <button onClick={() => this.handleHaroldClick()}>Harold</button>
+        <button onClick={() => this.handleDanielClick()}>Daniel</button>
+        </div>
+
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+          <h1>{this.state.name}</h1>
+          <img src={this.state.photo} />
+          <p>{this.state.description}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
